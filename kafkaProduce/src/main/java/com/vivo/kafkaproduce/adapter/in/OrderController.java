212f36d42684fb2b6cd1385d6f237a4b8943ce57
Willian Alberti <willalberti@gmail.com>
2021-11-16 11:12:59 -0300

    Correção:
    @GetMapping e @PostMapping sem "/"
    metodo search corrigido
    trocado @RequestParam por  o @PathVariable
    id como identity

---
+++ kafkaProduce/src/main/java/com/vivo/kafkaproduce/adapter/in/OrderController.java
@@ -0,0 +1,144 @@
+package com.vivo.kafkaproduce.adapter.in;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vivo.kafkaproduce.adapter.exception.AbstractBusinessException;
+import com.vivo.kafkaproduce.domain.OrderDto;
+import com.vivo.kafkaproduce.port.in.OrderPortIn;
+
+@RequestMapping("/orders")
+@RestController
+public class OrderController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  
+    @Autowired
+    private OrderPortIn orderPortIn;
+ 
+    @GetMapping
+    public ResponseEntity<List<OrderDto>> list() throws AbstractBusinessException {
+		
+    	try {
+    		
+    		List<OrderDto> lstDto = orderPortIn.list();
+        	
+        	return ResponseEntity.status(HttpStatus.OK).body(lstDto);
+        	
+		} catch (AbstractBusinessException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error("Class:{} metodo:{} Error:{}", this.getClass().getName(),Thread.currentThread().getStackTrace()[1].getMethodName(), e.getMessage() +" :: "+ e.getStackTrace());
+			throw new AbstractBusinessException(400, "Erro ao listar ordens");
+		}	    	
+    	
+    	
+    	
+    }    
+ 
+    @PostMapping
+    public ResponseEntity<OrderDto> create(@RequestBody @Valid OrderDto orderDto) throws AbstractBusinessException {
+    	
+    	try {
+    		OrderDto dto = orderPortIn.create(orderDto);
+        	
+    		return ResponseEntity.status(HttpStatus.OK).body(dto);
+        	
+		} catch (AbstractBusinessException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error("Class:{} metodo:{} Error:{}", this.getClass().getName(),Thread.currentThread().getStackTrace()[1].getMethodName(), e.getMessage() +" :: "+ e.getStackTrace());
+			throw new AbstractBusinessException(400, "Erro ao criar ordem");
+		}	 
+
+    }
+    
+    
+
+    
+    @GetMapping("/search")
+    public ResponseEntity<List<OrderDto>> search(
+    		@RequestParam(name = "max_total", required = false) Double max_total,
+    		@RequestParam(name = "min_total", required = false) Double min_total,
+    		@RequestParam(name = "status", required = false) String status,
+    		@RequestParam(name = "q", required = false) String q) throws AbstractBusinessException {
+    	try {
+        	
+    		if (max_total==null || max_total==null)
+    			throw new AbstractBusinessException(400, "informe os valores max_total e min_total");
+
+    		List<OrderDto> lstDto = orderPortIn.search(max_total, min_total, status, q);
+        	return ResponseEntity.status(HttpStatus.OK).body(lstDto);
+        	
+		} catch (AbstractBusinessException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error("Class:{} metodo:{} Error:{}", this.getClass().getName(),Thread.currentThread().getStackTrace()[1].getMethodName(), e.getMessage() +" :: "+ e.getStackTrace());
+			throw new AbstractBusinessException(400, "Erro ao procurar ordem");
+		}	  
+    }  
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDto> findById(@PathVariable Long id) throws AbstractBusinessException {
+    	try {
+    		OrderDto dto = orderPortIn.findById(id);
+        	
+        	return ResponseEntity.status(HttpStatus.OK).body(dto);
+        	
+		} catch (AbstractBusinessException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error("Class:{} metodo:{} Error:{}", this.getClass().getName(),Thread.currentThread().getStackTrace()[1].getMethodName(), e.getMessage() +" :: "+ e.getStackTrace());
+			throw new AbstractBusinessException(400, "Erro ao obter uma ordem");
+		}	 
+    }    
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderDto> update(@RequestBody @Valid OrderDto orderDto, @PathVariable Long id) throws AbstractBusinessException {
+    	try {
+    		OrderDto dto = orderPortIn.update(orderDto, id);
+        	
+        	return ResponseEntity.status(HttpStatus.OK).body(dto);
+        	
+		} catch (AbstractBusinessException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error("Class:{} metodo:{} Error:{}", this.getClass().getName(),Thread.currentThread().getStackTrace()[1].getMethodName(), e.getMessage() +" :: "+ e.getStackTrace());
+			throw new AbstractBusinessException(400, "Erro ao atualizar ordem");
+		}	 
+    }      
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<OrderDto> delete(@PathVariable Long id) throws AbstractBusinessException {
+    	try {
+    		OrderDto dto = orderPortIn.delete(id);
+        	
+        	return ResponseEntity.status(HttpStatus.OK).body(dto);
+        	
+		} catch (AbstractBusinessException e) {
+			throw e;
+		} catch (Exception e) {
+			logger.error("Class:{} metodo:{} Error:{}", this.getClass().getName(),Thread.currentThread().getStackTrace()[1].getMethodName(), e.getMessage() +" :: "+ e.getStackTrace());
+			throw new AbstractBusinessException(400, "Erro ao deletar ordem");
+		}	 
+    }     
+    
+
+}
+
---
+++ kafkaProduce/src/main/java/com/vivo/kafkaproduce/domain/OrderDto.java
@@ -0,0 +1,142 @@
+package com.vivo.kafkaproduce.domain;
+
+
+import java.util.Objects;
+
+
+import com.google.gson.annotations.SerializedName;
+
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2021-11-03T01:27:18.177Z")
+public class OrderDto {
+  
+  
+  @SerializedName("description")
+  private String description;
+
+  @SerializedName("id")
+  private Long id;
+
+  @SerializedName("name")
+  private String name;
+
+  @SerializedName("total")
+  private Double total;
+
+  @SerializedName("status")
+  private StatusEnum status;
+
+  
+  
+  public OrderDto description(String description) {
+    this.description = description;
+    return this;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public OrderDto id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public OrderDto name(String name) {
+    this.name = name;
+    return this;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public OrderDto total(Double total) {
+    this.total = total;
+    return this;
+  }
+
+  public Double getTotal() {
+    return total;
+  }
+
+  public void setTotal(Double total) {
+    this.total = total;
+  }
+
+  public OrderDto status(StatusEnum status) {
+    this.status = status;
+    return this;
+  }
+
+  public StatusEnum getStatus() {
+    return status;
+  }
+
+  public void setStatus(StatusEnum status) {
+    this.status = status;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    OrderDto orderDto = (OrderDto) o;
+    return Objects.equals(this.description, orderDto.description) &&
+        Objects.equals(this.id, orderDto.id) &&
+        Objects.equals(this.name, orderDto.name) &&
+        Objects.equals(this.total, orderDto.total) &&
+        Objects.equals(this.status, orderDto.status);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(description, id, name, total, status);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class OrderDto {\n");
+    
+    sb.append("    description: ").append(toIndentedString(description)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    total: ").append(toIndentedString(total)).append("\n");
+    sb.append("    status: ").append(toIndentedString(status)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
+}
